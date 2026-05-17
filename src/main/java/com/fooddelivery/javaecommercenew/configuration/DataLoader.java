ee5b5338f11e993930c22caa5e18a8a906a652e8
Nagarjunaracakunta <nagarjunarachakunta@gmail.com>
2026-05-18 03:11:00 +0530

    Update DataLoader.javadummy commit2

--- src/main/java/com/fooddelivery/javaecommercenew/configuration/DataLoader.java
+++ src/main/java/com/fooddelivery/javaecommercenew/configuration/DataLoader.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Component;
 public class DataLoader {
 
     public void runTasks(String... args) {
-        log.info("DataLoader is running on startup....{}", args.length > 0 ? String.join(", ", args) : "no args");
+        log.info("DataLoader is running on startup...{}", args.length > 0 ? String.join(", ", args) : "no args");
     }
 }
